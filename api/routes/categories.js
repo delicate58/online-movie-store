9d7bf9783d33feea28c6342a8e37266d292795a0
Sami Habtemeriam <sami.habtemeriam@ibm.com>
2019-09-26 21:16:38 +0100

    added frontend reat app using create-react-app

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,7 +1,6 @@
 const router=require("express").Router();
 
 const  authenticate=require("../middlewares/authenticate");
-
 router.get("/",authenticate,(req,res)=>{
    res.json({actegories:[{title:"category One"}]})
 });
